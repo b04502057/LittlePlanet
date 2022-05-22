5931a59c79d409c17365fcd2a1f13e0311889d59
Lu Cheng Yi <48717192+b04502057@users.noreply.github.com>
2022-05-22 14:53:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function myFunction0() {
+    var n1 = document.getElementById('sol').value;
+    var x1=document.getElementById('show');
+    var x2=document.getElementById('que');
+    if (n1=="v27890") {
+        x1.style.display = "block";
+        x2.style.display = "none"
+      } 
+      else if(n1=="T-shirt") {
+          alert("got you");
+      }
+      else if (n1=="fuck"){
+        alert("cool bro")
+      }
+      else if(n1=="pajamas"){
+        alert("right but... wrong")
+      }
+      else  {
+        alert("nice try");
+      }
+  }
